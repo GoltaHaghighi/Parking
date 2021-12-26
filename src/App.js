e5cbaf5b2e3585fe64c4d474cf8a11ace9571cc2
Golta Haghighi <goltaahaghighi@gamil.com>
2021-12-26 21:08:07 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+//Components
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Landing from "./components/Landing"
+import NotFound from './components/NotFound';
+import AboutUs from "./components/AboutUs";
+import Reservation from './components/Reservation';
+import Rules from './components/Rules';
+import Reserved from './components/Reserved';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/reservation" element={<Reservation />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/reserved" element={<Reserved />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/*" element={<Navigate to="/notfound" />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
